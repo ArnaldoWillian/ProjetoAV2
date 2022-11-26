d8dd0b4ec0446e10b0275f5db914957e33f5d058
Jonathas <jonathas673@gmail.com>
2022-11-26 10:56:20 -0300

    Add GET ONE obrainspecao

--- ProjObra/src/main/java/com/ProjetoProgAvan/ProjObra/Controller/ObraInspecaoController.java
+++ ProjObra/src/main/java/com/ProjetoProgAvan/ProjObra/Controller/ObraInspecaoController.java
@@ -3,12 +3,14 @@ package com.ProjetoProgAvan.ProjObra.Controller;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,10 +29,10 @@ public class ObraInspecaoController {
     public ObraInspecaoController(ObraInspecaoServices obraInspecaoServices) {
         this.obraInspecaoServices = obraInspecaoServices;
     }
-    
+
     @PostMapping
     public ResponseEntity<Object> saveProjObra(@RequestBody ObraInspecaoDot obraInspecaoDot) {
-       
+
         if (obraInspecaoServices.existsById(obraInspecaoDot.getId())) {
             return ResponseEntity.status(HttpStatus.CONFLICT).body("Conflict: ID is already in use!");
         }
@@ -47,7 +49,7 @@ public class ObraInspecaoController {
             return ResponseEntity.status(HttpStatus.CONFLICT).body("Conflict: Prioridade is already in use!");
         }
         if (obraInspecaoServices.existsByObraId(obraInspecaoDot.getObraId())) {
-            return ResponseEntity.status(HttpStatus.CONFLICT).body("Conflict: Observation is already registered for this Observation/Work Inspection id!");
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Conflict: ObraID is already in no");
         }
 
         var obraInspecao = new ObraInspecao();
@@ -55,8 +57,18 @@ public class ObraInspecaoController {
         ObraInspecao.setRegistrationDate(LocalDateTime.now(ZoneId.of("UTC")));
         return ResponseEntity.status(HttpStatus.CREATED).body(obraInspecaoServices.save(obraInspecao));
     }
+
     @GetMapping
     public ResponseEntity<List<ObraInspecao>> getAllProjObras() {
         return ResponseEntity.status(HttpStatus.OK).body(obraInspecaoServices.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getOneProjObra(@PathVariable(value = "id") Integer id) {
+        Optional<ObraInspecao> obraInspecaoOptional = obraInspecaoServices.findById(id);
+        if (!obraInspecaoOptional.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Obra not found.");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(obraInspecaoOptional.get());
     }
 }
