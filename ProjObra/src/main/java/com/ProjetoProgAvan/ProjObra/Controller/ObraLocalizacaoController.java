da15598c346b4ad84fb1a474a0a9b8dfd5332833
Jonathas <jonathas673@gmail.com>
2022-11-27 11:23:26 -0300

    Add PUT Obralocalizacao

--- ProjObra/src/main/java/com/ProjetoProgAvan/ProjObra/Controller/ObraLocalizacaoController.java
+++ ProjObra/src/main/java/com/ProjetoProgAvan/ProjObra/Controller/ObraLocalizacaoController.java
@@ -2,7 +2,6 @@ package com.ProjetoProgAvan.ProjObra.Controller;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.BeanUtils;
@@ -13,11 +12,13 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ProjetoProgAvan.ProjObra.dtos.ObraLocalizacaoDot;
+
 import com.ProjetoProgAvan.ProjObra.model.ObraLocalizacao;
 import com.ProjetoProgAvan.ProjObra.services.ObraLocalizacaoServices;
 
@@ -39,19 +40,19 @@ public class ObraLocalizacaoController {
             return ResponseEntity.status(HttpStatus.CONFLICT).body("Conflict: ID is already in use!");
         }
         if (obraLocalizacaoServices.existsByCidade(ObraLocalizacaoDot.getCidade())) {
-            return ResponseEntity.status(HttpStatus.CONFLICT).body("Conflict: DATE is already in use!");
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Conflict: Cidade is already in use!");
         }
         if (obraLocalizacaoServices.existsByEstado(ObraLocalizacaoDot.getEstado())) {
-            return ResponseEntity.status(HttpStatus.CONFLICT).body("Conflict: Observation is already registered for this Observation/Work Inspection id!");
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Conflict: Estado is already in use!");
         }
         if (obraLocalizacaoServices.existsByLatitude(ObraLocalizacaoDot.getLatitude())) {
-            return ResponseEntity.status(HttpStatus.CONFLICT).body("Conflict: DATE is already in use!");
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Conflict: Latitude is already in use!");
         }
         if (obraLocalizacaoServices.existsByLongitude(ObraLocalizacaoDot.getLongitude())) {
-            return ResponseEntity.status(HttpStatus.CONFLICT).body("Conflict: DATE is already in use!");
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Conflict: Longitude is already in use!");
         }
         if (obraLocalizacaoServices.existsByObraId(ObraLocalizacaoDot.getObraId())) {
-            return ResponseEntity.status(HttpStatus.CONFLICT).body("Conflict: DATE is already in use!");
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Conflict: ObraId is already in use!");
         }
 
         var obraLocalizacao = new ObraLocalizacao();
@@ -69,7 +70,7 @@ public class ObraLocalizacaoController {
     public ResponseEntity<Object> getOneProjObra(@PathVariable(value = "id") Integer id) {
         Optional<ObraLocalizacao> ObraLocalizacaoOptional = obraLocalizacaoServices.findById(id);
         if (!ObraLocalizacaoOptional.isPresent()) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Obra not found.");
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("ObraLocalizacao not found.");
         }
         return ResponseEntity.status(HttpStatus.OK).body(ObraLocalizacaoOptional.get());
     }
@@ -82,5 +83,23 @@ public class ObraLocalizacaoController {
         }
         obraLocalizacaoServices.delete(obraLocalizacaoOptional.get());
         return ResponseEntity.status(HttpStatus.OK).body(" ObObraLocalizacaora deleted successfully.");
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> updateProjObra(@PathVariable(value = "id") Integer id,
+            @RequestBody ObraLocalizacaoDot obraLocalizacaoDot) {
+        Optional<ObraLocalizacao> obraLocalizacaoOptional = obraLocalizacaoServices.findById(id);
+        if (!obraLocalizacaoOptional.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("ObraLocalizacaora Spot not found.");
+        }
+        var obraLocalizacao = obraLocalizacaoOptional.get();
+        obraLocalizacao.setId(obraLocalizacaoDot.getId());
+        obraLocalizacao.setCidade(obraLocalizacaoDot.getCidade());
+        obraLocalizacao.setEstado(obraLocalizacaoDot.getEstado());
+        obraLocalizacao.setLatitude(obraLocalizacaoDot.getLatitude());
+        obraLocalizacao.setLongitude(obraLocalizacaoDot.getLongitude());
+        obraLocalizacao.setObraId(obraLocalizacaoDot.getObraId());
+
+        return ResponseEntity.status(HttpStatus.OK).body(obraLocalizacaoServices.save(obraLocalizacao));
     }
 }
